7f799cb158a1bd6540441c7d7ed1a65941d74dac
jay <beomgolaeda@gmail.com>
2025-04-04 00:40:30 +0900

    2025/04/04

--- src/main/java/com/example/hellocontroller/HelloControllerApplication.java
+++ src/main/java/com/example/hellocontroller/HelloControllerApplication.java
@@ -2,7 +2,7 @@ package com.example.hellocontroller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//프로젝트 이름 + Application : 프로그램을 시작을 담당하는 파일
 @SpringBootApplication
 public class HelloControllerApplication {
 
