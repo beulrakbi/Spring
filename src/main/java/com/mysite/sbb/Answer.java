c83a146a5b274cf42608f9863fb8e591653a53b7
jay <beomgolaeda@gmail.com>
2025-04-05 13:49:37 +0900

    2025/04/05 h2 엔티티 등록 완료

---
+++ src/main/java/com/mysite/sbb/Answer.java
@@ -0,0 +1,24 @@
+package com.mysite.sbb;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @ManyToOne //게시판 서비스에서는 하나의 질문에 답변은 여러개 달린다. 따라서 답변은 Many 질문은 One 이다. (N:1관계를 나타냄)
+    private Question question;
+}
--- src/main/java/com/mysite/sbb/Question.java
+++ src/main/java/com/mysite/sbb/Question.java
@@ -5,23 +5,25 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Setter
 @Entity
 public class Question {
-    @Id
-    //id속성을 기본키로 지정한다.
-    //id 속성의 고유 번호들은 엔티티에서 각 데이터들을 구분하는 유효한 값으로, 중복되면 안 되기 때문이다.
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    
+    @Id //@Id : id속성을 기본키로 지정한다.
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // GeneratedValue  :  1씩 자동으로 증가 기본키는 중복되면 안 되기 때문에
     private Integer id;
 
-    @Column(length = 200)
+    //Column : 엔티티의 속성은 테이블의 열 이름과 일치한다. 세부 설정을 위해 어노테이션을 사용 (필드 이름과 다를 때 연결하는 역할도 함)
+    @Column(length = 200) //length : 길이를 설정
+    private String subject;
+
+    @Column(columnDefinition = "TEXT") //columnDefinition : 열 데이터의 유형이나 성격을 정의할 때 사용
     private String content;
 
-    @Column(columnDefinition = "TEXT")
-    private String conten;
-
     private LocalDateTime createDate;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE) //mappedBy : 참조 엔티티의 속성명을 정의 //cascade Type.REMOVE : 질문을 삭제하면 그에 달린 댓글도 삭제
+    private List<Answer> answerList;
 }
