4dd1e81d4e4e72c004d9518b45bd6be842301082
jay <beomgolaeda@gmail.com>
2025-04-07 22:34:02 +0900

    2025-4-7

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -3,50 +3,34 @@ package com.mysite.sbb;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 public class SbbApplicationTests {
 
     @Autowired
     private QuestionRepository questionRepository;
- 
+
+    @Autowired
+    private AnswerRepository answerRepository;
+
+    @Transactional
     @Test
     void testJpa(){
-//        Question q1 = new Question();
-//        q1.setSubject("sbb가 무엇인가요?");
-//        q1.setContent("sbb에 대해서 알고 싶습니다.");
-//        q1.setCreateDate(LocalDateTime.now());
-//        this.questionRepository.save(q1);
-//
-//
-//        Question q2 = new Question();
-//        q2.setSubject("스프링 부트 모델 질문입니다.");
-//        q2.setContent("id는 자동으로 생성되나요?");
-//        q2.setCreateDate(LocalDateTime.now());
-//        this.questionRepository.save(q2);
+        Optional<Question> optionalQuestion = this.questionRepository.findById(2);
+        assertTrue(optionalQuestion.isPresent());
+        Question question = optionalQuestion.get();
 
-//        List<Question> all = this.questionRepository.findAll();
-//        assertEquals(2, all.size());
-//
-//        Question q = all.get(0);
-//        assertEquals("sbb가 무엇인가요?", q.getSubject());
+        List<Answer> answerList = question.getAnswerList();
 
-        // 데이터베이스의 ID 값을 활용해서 데이터 조회
-//        Optional<Question> oq = this.questionRepository.findById(1);
-//        if(oq.isPresent()){
-//            Question q = oq.get();
-//            assertEquals("sbb가 무엇인가요?", q.getSubject());
-//        }
-
-        Question question = this.questionRepository.findBySubject("sbb가 무엇인가요?");
-        assertEquals(1, question.getId());
-
-
+        assertEquals(1, answerList.size());
+        assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
     }
 }
